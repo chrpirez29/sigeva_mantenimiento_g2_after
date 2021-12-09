1f0aea0de03a289f67abbd4769f79dfd9fd48bc1
Christopher Pirez <Christopher.Pirez@alu.uclm.es>
2021-12-09 12:18:07 +0100

    Cogemos bien el id del usuario seleccionado en la lista de usuarios.

--- SIGEVA-UI/src/app/contenedor-citasPersonal/contenedor-citasPersonal.component.ts
+++ SIGEVA-UI/src/app/contenedor-citasPersonal/contenedor-citasPersonal.component.ts
@@ -1,41 +1,50 @@
-import {Component, OnInit} from '@angular/core';
-import {JsonService} from "../Service/json.service";
-import {TokenService} from "../Service/token.service";
-import {HttpParams} from "@angular/common/http";
-import {CitaConObjetos} from "../Model/cita-con-objetos";
+import { Component, OnInit } from '@angular/core';
+import { JsonService } from "../Service/json.service";
+import { TokenService } from "../Service/token.service";
+import { HttpParams } from "@angular/common/http";
+import { CitaConObjetos } from "../Model/cita-con-objetos";
 
 @Component({
-  selector: 'app-contenedor-citasPersonal',
-  templateUrl: './contenedor-citasPersonal.component.html',
-  styleUrls: ['./contenedor-citasPersonal.component.css']
+	selector: 'app-contenedor-citasPersonal',
+	templateUrl: './contenedor-citasPersonal.component.html',
+	styleUrls: ['./contenedor-citasPersonal.component.css']
 })
 export class ContenedorCitasPersonalComponent implements OnInit {
 
-  citas: CitaConObjetos[] = [];
-  idUsuario: string;
-  msgNoCitas = "";
+	citas: CitaConObjetos[] = [];
+	idUsuario: string;
+	msgNoCitas = "";
 
-  constructor(private json: JsonService, private tokenService: TokenService) {
-    this.idUsuario = String("c4af4126-76f0-4e1e-883e-89b7e5704d88");
-  }
+	constructor(private json: JsonService, private tokenService: TokenService) {
+		this.idUsuario = String(this.getId());
+	}
+	
+	ngOnInit(): void {
+	}
 
-  ngOnInit(): void {
-    this.getCitas();
-  }
+	getCitas(id:string) {
 
-  getCitas() {
-    let params = new HttpParams({
-      fromObject: {
-        idPaciente: String("c4af4126-76f0-4e1e-883e-89b7e5704d88")
-      }
-    });
-    this.json.getJsonPJ('/cita/obtenerCitasFuturasDelPaciente', params).subscribe(
-      data => {
-        this.citas = data;
-        if (this.citas.length == 0) {
-          this.msgNoCitas = "No hay citas asignadas";
-        }
-      }
-    );
-  }
+		let params = new HttpParams({
+			fromObject: {
+				idPaciente: String(id)
+			}
+		});
+		this.json.getJsonPJ('/cita/obtenerCitasFuturasDelPaciente', params).subscribe(
+			data => {
+				this.citas = data;
+				if (this.citas.length == 0) {
+					this.msgNoCitas = "No hay citas asignadas";
+				}
+			}
+		);
+	}
+
+	getId(): any {
+		this.json.getJson('/tokenUsuario/getId').subscribe(
+			id => {
+				this.getCitas(id)
+			}
+		);
+		;
+	}
 }
